36743b18816ef461cc80b923ab5cd0191a472d4a
Tiago Rodrigues <tiago.rodrigues.ee@gmail.com>
2021-05-25 20:58:19 -0300

    Menu ok
    Menu fixed and now works.

--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -1,34 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class StartGame : MonoBehaviour
 {
-    public string sceneEasy;
-    public string sceneMedium;
-    public string sceneHard;
-    public string sceneCredits;
 
-    Dropdown dropDown;
+    public string sceneCredits;
+    public static int dificuldade;
+    
+    Dictionary<int, string> cenas = new Dictionary<int, string>(){{0, "EasyGame"}, {1, "MediumGame"}, {2, "HardGame"}};
     
     public void StarGame()
     {
-        dropDown = GetComponent<Dropdown>();
-        int val = dropDown.value;
-     
-
-        if (val == 0)
-        {
-            SceneManager.LoadScene(sceneEasy);    
-        }
-         if (val == 1)
-        {
-            SceneManager.LoadScene(sceneMedium);    
-        }
-         if (val == 2)
-        {
-            SceneManager.LoadScene(sceneHard);    
-        }
+        SceneManager.LoadScene(cenas[dificuldade]);    
     }
 
     public void Credits()
